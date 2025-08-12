d5a20a61722ded3ae8fd9dae33880ed57ec90b96
Demorck <57636452+Demorck@users.noreply.github.com>
2025-08-12 02:35:30 +0200

    first commit

---
+++ scripts/init.lua
@@ -0,0 +1,14 @@
+DEBUG = false
+ENABLE_DEBUG_LOG = DEBUG
+
+
+Tracker:AddItems("items/items.json")
+
+Tracker:AddMaps("maps/maps.json")
+
+Tracker:AddLocations("locations/maps.json")
+Tracker:AddLocations("locations/continent.json")
+
+Tracker:AddLayouts("layouts/items.json")
+Tracker:AddLayouts("layouts/maps.json")
+Tracker:AddLayouts("layouts/tracker.json")

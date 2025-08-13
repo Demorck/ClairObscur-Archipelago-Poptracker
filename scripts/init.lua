fc048b220d31fce0af84a2ae159ea2eb1b5e0881
Demorck <57636452+Demorck@users.noreply.github.com>
2025-08-13 05:29:54 +0200

    Adding Spring Meadows & Flying Waters

--- scripts/init.lua
+++ scripts/init.lua
@@ -6,8 +6,12 @@ Tracker:AddItems("items/items.json")
 
 Tracker:AddMaps("maps/maps.json")
 
-Tracker:AddLocations("locations/maps.json")
-Tracker:AddLocations("locations/continent.json")
+-- Tracker:AddLocations("locations/maps.json")
+-- Tracker:AddLocations("locations/continent.json")
+Tracker:AddLocations("locations/spring_meadows.json")
+Tracker:AddLocations("locations/logics/spring_meadows.json")
+Tracker:AddLocations("locations/flying_waters.json")
+Tracker:AddLocations("locations/logics/flying_waters.json")
 
 Tracker:AddLayouts("layouts/items.json")
 Tracker:AddLayouts("layouts/maps.json")

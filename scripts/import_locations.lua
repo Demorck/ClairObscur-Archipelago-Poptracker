3f87e56e593d7cbba81b0533c843149a961a4cfc
Demorck <57636452+Demorck@users.noreply.github.com>
2025-08-13 17:57:43 +0200

    Gestral village & Ancient sanctuary

---
+++ scripts/import_locations.lua
@@ -0,0 +1,19 @@
+-- The folder "locations" are locations that we see on maps (the square & trapezoid).
+-- In the root of locations, that's the real location on maps (the coordinates) with a reference to the logic (so, in logics folder)
+
+
+-- Spring meadows
+Tracker:AddLocations("locations/spring_meadows.json")
+Tracker:AddLocations("locations/logics/spring_meadows.json")
+
+-- Flying Waters
+Tracker:AddLocations("locations/flying_waters.json")
+Tracker:AddLocations("locations/logics/flying_waters.json")
+
+-- Ancient sanctuary
+Tracker:AddLocations("locations/ancient_sanctuary.json")
+Tracker:AddLocations("locations/logics/ancient_sanctuary.json")
+
+-- Gestral village
+Tracker:AddLocations("locations/gestral_village.json")
+Tracker:AddLocations("locations/logics/gestral_village.json")
--- scripts/init.lua
+++ scripts/init.lua
@@ -8,10 +8,7 @@ Tracker:AddMaps("maps/maps.json")
 
 -- Tracker:AddLocations("locations/maps.json")
 -- Tracker:AddLocations("locations/continent.json")
-Tracker:AddLocations("locations/spring_meadows.json")
-Tracker:AddLocations("locations/logics/spring_meadows.json")
-Tracker:AddLocations("locations/flying_waters.json")
-Tracker:AddLocations("locations/logics/flying_waters.json")
+ScriptHost:LoadScript("scripts/import_locations.lua")
 
 Tracker:AddLayouts("layouts/items.json")
 Tracker:AddLayouts("layouts/maps.json")
